e18b3c7e6c3ba7f7e62884e1a0d2309e2e404cd1
Entervive <aleksander.staszkow@entervive.pl>
2025-02-20 13:22:48 +0100

    Update program1.py

--- Programowanie obiektowe/Chojnacki/program1.py
+++ Programowanie obiektowe/Chojnacki/program1.py
@@ -1,9 +1,13 @@
 class Uczen:
     #Specjalna funkcja inicjalizująca obiekt
     def __init__(self, imie, nazwisko, wiek):
-        self.imie = imie
+        self.imie = imie #Domyślne dane self.imie = "Ania"
         self.nazwisko = nazwisko
         self.wiek = wiek
 
     def przedstaw_sie(self):
         print(f'Nazywam się {self.imie} {self.nazwisko} i mam {self.wiek} lat.')
+
+ucz1=Uczen('Jan', 'Kowalski', 15)
+print(ucz1.imie)
+ucz1.przedstaw_sie()
